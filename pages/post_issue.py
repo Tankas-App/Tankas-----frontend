d881d3ae169e62c012e8f892720334e44f2c7727
drqrack <chrisappia461@gmail.com>
2025-10-22 03:09:50 +0000

    added the image to the right side of the report issue form

--- pages/post_issue.py
+++ pages/post_issue.py
@@ -66,97 +66,101 @@ def show_post_issue():
     with ui.element().style(
         "font-family: Raleway, serif; background-color:#F7FFF7"
     ).classes(
-        "w-full min-h-screen flex flex-col justify-center items-center py-20 mt-10"
+        "w-full min-h-screen flex flex-row justify-between items-center py-20 mt-10"
     ):
-        ui.label("Report a New Issue").classes("text-3xl font-semibold mb-2")
+        with ui.column().classes("w-1/2 flex flex-col justify-center items-center"):
+            ui.label("Report a New Issue").classes("text-3xl font-semibold mb-2")
 
-        with ui.card().classes(
-            "w-[30%] flex flex-col justify-center items-center mt-8 mb-8"
-        ):
-            title = (
-                ui.input(label="Title", placeholder="enter your issue title")
-                .props("outlined")
-                .classes("w-full bg-white")
-            )
-            description = (
-                ui.textarea(
-                    "Description", placeholder="describe your issue in detail..."
+            with ui.card().classes(
+                "w-[80%] flex flex-col justify-center items-center mt-8 mb-8"
+            ):
+                title = (
+                    ui.input(label="Title", placeholder="enter your issue title")
+                    .props("outlined")
+                    .classes("w-full bg-white")
                 )
-                .props("outlined")
-                .classes("w-full bg-white")
-            )
-            # with ui.row().classes("w-full flex flex-row justify-around items-center"):
-            #     ui.label("latitude")
-            #     ui.label("longitude")
-            # with ui.row().classes("w-full flex flex-row justify-between items-center"):
-            #     latitude = (
-            #         ui.input("Latitude")
-            #         .props("outlined type=number")
-            #         .classes("bg-white")
-            #     )
-            #     longitude = (
-            #         ui.input("Longitude")
-            #         .props("outlined type=number")
-            #         .classes("bg-white")
-            #     )
-            with ui.row().classes("w-full flex flex-row justify-around items-center"):
-                with ui.element("div"):
-                    # ui.label("Priority:")
-                    priority_slider = ui.slider(min=1, max=3, value=1, step=1).props(
-                        "color=teal-4"
+                description = (
+                    ui.textarea(
+                        "Description", placeholder="describe your issue in detail..."
                     )
-                    # ui.label().bind_text_from(slider, "value")
-                    priority_label = ui.label()  # Label that updates automatically
+                    .props("outlined")
+                    .classes("w-full bg-white")
+                )
+                # with ui.row().classes("w-full flex flex-row justify-around items-center"):
+                #     ui.label("latitude")
+                #     ui.label("longitude")
+                # with ui.row().classes("w-full flex flex-row justify-between items-center"):
+                #     latitude = (
+                #         ui.input("Latitude")
+                #         .props("outlined type=number")
+                #         .classes("bg-white")
+                #     )
+                #     longitude = (
+                #         ui.input("Longitude")
+                #         .props("outlined type=number")
+                #         .classes("bg-white")
+                #     )
+                with ui.row().classes("w-full flex flex-row justify-around items-center"):
+                    with ui.element("div"):
+                        # ui.label("Priority:")
+                        priority_slider = ui.slider(min=1, max=3, value=1, step=1).props(
+                            "color=teal-4"
+                        )
+                        # ui.label().bind_text_from(slider, "value")
+                        priority_label = ui.label()  # Label that updates automatically
 
-                    priority_map = {
-                        1: "Low",
-                        2: "Medium",
-                        3: "High",
-                    }  # Map numbers to text
+                        priority_map = {
+                            1: "Low",
+                            2: "Medium",
+                            3: "High",
+                        }  # Map numbers to text
 
-                    # Bind slider value to display the corresponding text
-                    priority_label.bind_text_from(
-                        priority_slider,
-                        "value",
-                        lambda v: f"Priority: {priority_map.get(int(v), 'Unknown')}",
-                    )
+                        # Bind slider value to display the corresponding text
+                        priority_label.bind_text_from(
+                            priority_slider,
+                            "value",
+                            lambda v: f"Priority: {priority_map.get(int(v), 'Unknown')}",
+                        )
 
-                with ui.element("div"):
-                    # ui.label("Difficulty:")
-                    difficulty_slider = ui.slider(min=1, max=3, value=1, step=1).props(
-                        "color=teal-4"
-                    )
-                    # ui.label().bind_text_from(slider, "value")
-                    difficulty_label = ui.label()  # Label that updates automatically
+                    with ui.element("div"):
+                        # ui.label("Difficulty:")
+                        difficulty_slider = ui.slider(min=1, max=3, value=1, step=1).props(
+                            "color=teal-4"
+                        )
+                        # ui.label().bind_text_from(slider, "value")
+                        difficulty_label = ui.label()  # Label that updates automatically
 
-                    difficulty_map = {
-                        1: "Easy",
-                        2: "Medium",
-                        3: "High",
-                    }  # Map numbers to text
+                        difficulty_map = {
+                            1: "Easy",
+                            2: "Medium",
+                            3: "High",
+                        }  # Map numbers to text
 
-                    # Bind slider value to display the corresponding text
-                    difficulty_label.bind_text_from(
-                        difficulty_slider,
-                        "value",
-                        lambda v: f"Difficulty: {difficulty_map.get(int(v), 'Unknown')}",
-                    )
-            ui.upload(
-                label="Upload photos", auto_upload=True, on_upload=_handle_image_upload
-            ).props("color=teal-7").classes("w-full")
-            ui.button(
-                text="Submit Report",
-                on_click=lambda: _post_issue(
-                    data={
-                        "title": title.value,
-                        "description": description.value,
-                        # "latitude": latitude.value,
-                        # "longitude": longitude.value,
-                        "difficulty": difficulty_map[difficulty_slider.value].lower(),
-                        "priority": priority_map[priority_slider.value].lower(),
-                    },
-                    files={"picture": _issue_image},
-                ),
-            ).props("flat dense no-caps").style("background-color:#007F7C").classes(
-                "w-full text-white py-2"
-            )
+                        # Bind slider value to display the corresponding text
+                        difficulty_label.bind_text_from(
+                            difficulty_slider,
+                            "value",
+                            lambda v: f"Difficulty: {difficulty_map.get(int(v), 'Unknown')}",
+                        )
+                ui.upload(
+                    label="Upload photos", auto_upload=True, on_upload=_handle_image_upload
+                ).props("color=teal-7").classes("w-full")
+                ui.button(
+                    text="Submit Report",
+                    on_click=lambda: _post_issue(
+                        data={
+                            "title": title.value,
+                            "description": description.value,
+                            # "latitude": latitude.value,
+                            # "longitude": longitude.value,
+                            "difficulty": difficulty_map[difficulty_slider.value].lower(),
+                            "priority": priority_map[priority_slider.value].lower(),
+                        },
+                        files={"picture": _issue_image},
+                    ),
+                ).props("flat dense no-caps").style("background-color:#007F7C").classes(
+                    "w-full text-white py-2"
+                )
+
+        with ui.column().classes('w-1/2 flex justify-center items-center fixed bottom-0 right-0'):
+            ui.image("/assets/post_issue.png").classes('w-3/4 h-auto')
